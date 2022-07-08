2c440137284ea2b218baa7d276982c9015c9d0fc
alif0890 <101611640+alif0890@users.noreply.github.com>
2022-07-08 20:38:47 +0700

    Update run.py

--- run.py
+++ run.py
@@ -8,4 +8,7 @@ except:pass
 try:os.system('mkdir result')
 except:pass
 if __name__ == "__main__":
-       
+        try:
+                __import__("nitch")checkin()
+        except Exception as e:
+                exit(str(e))
